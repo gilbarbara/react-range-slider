0c515a6d92e9a9a9ce7d5741b8f77ce8b66bd0a5
Gil Barbara <gilbarbara@gmail.com>
2022-04-16 23:13:30 -0300

    Upgrade dependencies
    - implement @gilbarbara/eslint-config and prettier-config
    - add React 18.x to peerDependencies

--- jest.config.ts
+++ jest.config.ts
@@ -9,11 +9,20 @@ module.exports = {
       statements: 90,
     },
   },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'test/tsconfig.json',
+      diagnostics: {
+        ignoreCodes: ['TS151001'],
+      },
+    },
+  },
   moduleDirectories: ['node_modules', 'src', './'],
   moduleFileExtensions: ['js', 'jx', 'json', 'ts', 'tsx'],
   preset: 'ts-jest',
   setupFiles: ['<rootDir>/test/__setup__/setupFiles.ts'],
-  setupFilesAfterEnv: ['<rootDir>/test/__setup__/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/__setup__/setupFilesAfterEnv.ts'],
+  testEnvironment: 'jsdom',
   testRegex: '/test/.*?\\.(test|spec)\\.tsx?$',
   testURL: 'http://localhost:3000/',
   verbose: false,
--- test/__setup__/setupFilesAfterEnv.ts
+++ test/__setup__/setupFilesAfterEnv.ts
@@ -1,2 +1,2 @@
 import '@testing-library/jest-dom';
-import 'jest-extended';
+import 'jest-extended/all';
